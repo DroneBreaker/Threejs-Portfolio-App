13c87b6281a5aceb91e1251fd48ede0f10592ec7
DroneBreaker <brownierz01@gmail.com>
2023-02-24 12:58:24 +0000

    Adding more functionalities

--- main.js
+++ main.js
@@ -1,35 +1,194 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
+import gsap from 'gsap'
 
+const gui = new dat.GUI()
+const world = {
+  plane: {
+    width: 18,
+    height: 18, 
+    widthSegments: 24, 
+    heightSegments: 24
+  }
+}
+
+gui.add(world.plane, 'width', 1, 50).onChange(generatePlane)
+gui.add(world.plane, 'height', 1, 50).onChange(generatePlane)
+gui.add(world.plane, 'widthSegments', 1, 50).onChange(generatePlane)
+gui.add(world.plane, 'heightSegments', 1, 50).onChange(generatePlane)
+
+function generatePlane() {
+  mesh.geometry.dispose()
+  mesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width, world.plane.height, world.plane.widthSegments, world.plane.heightSegments
+  )
+    
+
+  const { array } = mesh.geometry.attributes.position
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i]
+    const y = array[i + 1]
+    const z = array[i + 2]
+
+    array[i + 2] = z + Math.random()
+  }
+
+  const colors = []
+  for (let i = 0; i < mesh.geometry.attributes.position.count; i++) {
+    colors.push(0, 0.19, 0.4)
+  }
+
+  mesh.geometry.setAttribute('color', new THREE.BufferAttribute(
+    new Float32Array(colors), 3)
+  )
+}
+
+const raycaster = new THREE.Raycaster()
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, innerWidth/ innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(95, window.innerWidth/ window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.PlaneGeometry(7, 7, 10, 10)
-const material = new THREE.MeshPhongMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+const geometry = new THREE.PlaneGeometry(24, 24, 30, 30)
+const material = new THREE.MeshPhongMaterial({
+  side: THREE.DoubleSide, 
+  flatShading: THREE.FlatShading,
+  vertexColors: true
+ })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+new OrbitControls(camera, renderer.domElement)
 camera.position.z = 5
 
+
+//vertice position random
 const { array } = mesh.geometry.attributes.position
-for (let i = 0; i < array.length; i++) {
-  const element = array[i];
+const randomValues = []
+for (let i = 0; i < array.length; i ++) {
+  if(i % 3 === 0) { 
+    const x = array[i]
+    const y = array[i + 1]
+    const z = array[i + 2]
+
+    array[i] = x + (Math.random() - 0.5) * 1.2
+    array[i + 1] = y + (Math.random() - 0.5) * 1.2
+    array[i + 2] = z + Math.random()
+  }
+  randomValues.push(Math.random())
 }
+
+mesh.geometry.attributes.position.randomValues = randomValues
+mesh.geometry.attributes.position.originalPosition = mesh.geometry.attributes.position.array
+
+//color attribute addition
+const colors = []
+for (let i = 0; i < mesh.geometry.attributes.position.count; i++) {
+  colors.push(0, 0.19, 0.4)
+}
+
+mesh.geometry.setAttribute('color', new THREE.BufferAttribute(
+  new Float32Array(colors), 3)
+)
 
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(0, 0, 1)
 scene.add(light)
 
+const backLight = new THREE.DirectionalLight(0xffffff, 1)
+backLight.position.set(0, 0, -1)
+scene.add(light)
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+let frame = 0
+
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
+  raycaster.setFromCamera(mouse, camera)
+  frame += 0.01
+
+  const { array, originalPosition, randomValues } = mesh.geometry.attributes.position
+  for (let i = 0; i < array.length; i += 3) {
+    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01
+  }
+  mesh.geometry.attributes.position.needsUpdate = true
+
+  const intersects = raycaster.intersectObject(mesh)
+
+  if(intersects.length > 0) {
+    const { color } = intersects[0].object.geometry.attributes
+
+    //vertice 1
+    color.setX(intersects[0].face.a, 0.1)
+    color.setY(intersects[0].face.a, 0.5)
+    color.setZ(intersects[0].face.a, 1)
+
+    //vertice 2
+    color.setX(intersects[0].face.b, 0.1)
+    color.setY(intersects[0].face.b, 0.5)
+    color.setZ(intersects[0].face.b, 1)
+
+    //vertice 3
+    color.setX(intersects[0].face.c, 0.1)
+    color.setY(intersects[0].face.c, 0.5)
+    color.setZ(intersects[0].face.c, 1)
+
+    intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+    const initialColor = {
+      r: 0,
+      g: .19,
+      b: .4
+    }
+
+    const hoverColor = {
+      r: .1,
+      g: .5,
+      b: 1
+    }
+
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      onUpdate: () => {
+        //vertice 1
+        color.setX(intersects[0].face.a, hoverColor.r)
+        color.setY(intersects[0].face.a, hoverColor.g)
+        color.setZ(intersects[0].face.a, hoverColor.b)
+
+        //vertice 2
+        color.setX(intersects[0].face.b, hoverColor.r)
+        color.setY(intersects[0].face.b, hoverColor.g)
+        color.setZ(intersects[0].face.b, hoverColor.b)
+
+        //vertice 3
+        color.setX(intersects[0].face.c, hoverColor.r)
+        color.setY(intersects[0].face.c, hoverColor.g)
+        color.setZ(intersects[0].face.c, hoverColor.b)
+        color.needsUpdate = true
+      }
+    })
+  }
   // mesh.rotation.x += 0.02
 }
 animate()
+
+
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1
+})
 
 // console.log(scene)
 // console.log(camera)
